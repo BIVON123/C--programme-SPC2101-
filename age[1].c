ee1f44636e917f3736bb82e7a40ccd142c358ab1
Bivon <bivonnyandiko6@gmail.com>
2025-09-19 13:06:30 +0300

    Add files via upload

---
+++ age[1].c
@@ -0,0 +1,29 @@
+//simple Hello world program
+//single line comment
+/*
+Name:
+Reg no:
+Description:softwaengineering units 
+
+*/
+#include<stdio.h> //prepcessor directive:scanf(),printf("");
+//main function
+
+
+
+
+int main() {
+
+    int age;
+    printf("enter your age:");
+    scanf("%d", &age);
+    printf("your age is %d", age);
+    
+
+    return 0;
+}
+
+
+
+
+    
