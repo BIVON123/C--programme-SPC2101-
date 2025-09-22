0d5bd12d7317cc6bce477e3a2c12d9b86a27c4ef
Bivon <bivonnyandiko6@gmail.com>
2025-09-22 15:37:31 +0300

    Add files via upload

---
+++ annual income.cpp
@@ -0,0 +1,27 @@
+   
+   /*
+Name:BIVON NYANDIKO
+Reg no:PA106/G/28825/25
+Description:writing a python program impementing some requirements
+*/
+#include<stdio.h>
+int main(){
+
+    float income; //%f
+    int age; //%d
+
+    printf("please enter your age:");
+    scanf("%d",&age);
+
+    printf("please enter your annual income:");
+    scanf("%f",&income);
+    
+    if(age>=21 && income>=21000){
+    printf(" congratulations!you qualify for a loan \n");
+	}
+    else{
+	printf("unfortunately,we are unable to offer you a loan at this time \n");
+	}
+
+    return 0;
+}
